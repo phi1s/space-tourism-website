87b9b26dfdb9223cfa71ef37139499725c2935e6
scnp <scnp@zhaw.ch>
2024-05-20 15:33:18 +0200

    wip: read data from json

--- src/views/DestinationView.vue
+++ src/views/DestinationView.vue
@@ -1,25 +1,36 @@
+<script setup lang="ts">
+import { destinations } from "../data";
+import { ref } from 'vue'
+
+let activeView = ref(0);
+
+const changeActiveView = (view: number) => {
+    activeView.value = view;
+};
+
+</script>
+
 <template>
     <div class="destination">
+        {{ activeView }}
         <h4><b>01 </b>PICK YOUR DESTINATION</h4>
         <img src="../assets/destination/image-moon.png" />
         <ol>
-            <li>MOON</li>
-            <li>MARS</li>
-            <li>EUROPA</li>
-            <li>TITAN</li>
+            <li><button @click="changeActiveView(0)">MOON</button></li>
+            <li><button @click="changeActiveView(1)">MARS</button></li>
+            <li><button @click="changeActiveView(2)">EUROPA</button></li>
+            <li><button @click="changeActiveView(3)">TITAN</button></li>
         </ol>
-        <h1>MOON</h1>
-        <p>See our planet as you’ve never seen it before. A perfect relaxing trip away to help regain perspective and
-            come back refreshed. While you’re there, take in some history by visiting the Luna 2 and Apollo 11 landing
-            sites.</p>
+        <h1>{{ destinations[activeView].name }}</h1>
+        <p>{{ destinations[activeView].description }}</p>
         <hr />
         <div class="stats-item">
             <h6>AVG. DISTANCE</h6>
-            <h5>384,400 km</h5>
+            <h5>{{ destinations[activeView].distance }}</h5>
         </div>
         <div class="stats-item">
             <h6>Est. travel time</h6>
-            <h5>3 days</h5>
+            <h5>{{ destinations[activeView].travel }}</h5>
         </div>
         <div class="background" />
     </div>
