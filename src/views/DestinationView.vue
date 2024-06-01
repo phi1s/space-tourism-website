3d2c0e769917c63ac357ac7a59290c2ca578567a
scnp <scnp@zhaw.ch>
2024-06-01 20:38:41 +0200

    first draft of responsive design for home and destination

--- src/views/DestinationView.vue
+++ src/views/DestinationView.vue
@@ -17,23 +17,32 @@ const isActive = (view: number) => {
 <template>
     <div class="destination">
         <h4><b>01 </b>PICK YOUR DESTINATION</h4>
-        <img :src="destinations[activeView].images.png" />
-        <ol>
-            <li :class="{ 'active': isActive(0) }"><button @click="changeActiveView(0)">MOON</button></li>
-            <li :class="{ 'active': isActive(1) }"><button @click="changeActiveView(1)">MARS</button></li>
-            <li :class="{ 'active': isActive(2) }"><button @click="changeActiveView(2)">EUROPA</button></li>
-            <li :class="{ 'active': isActive(3) }"><button @click="changeActiveView(3)">TITAN</button></li>
-        </ol>
-        <h1>{{ destinations[activeView].name }}</h1>
-        <p>{{ destinations[activeView].description }}</p>
-        <hr />
-        <div class="stats-item">
-            <h6>AVG. DISTANCE</h6>
-            <h5>{{ destinations[activeView].distance }}</h5>
-        </div>
-        <div class="stats-item">
-            <h6>Est. travel time</h6>
-            <h5>{{ destinations[activeView].travel }}</h5>
+        <div class="container">
+            <div class="item">
+
+                <img :src="destinations[activeView].images.png" />
+            </div>
+            <div class="item">
+                <ol>
+                    <li :class="{ 'active': isActive(0) }"><button @click="changeActiveView(0)">MOON</button></li>
+                    <li :class="{ 'active': isActive(1) }"><button @click="changeActiveView(1)">MARS</button></li>
+                    <li :class="{ 'active': isActive(2) }"><button @click="changeActiveView(2)">EUROPA</button></li>
+                    <li :class="{ 'active': isActive(3) }"><button @click="changeActiveView(3)">TITAN</button></li>
+                </ol>
+                <h1>{{ destinations[activeView].name }}</h1>
+                <p>{{ destinations[activeView].description }}</p>
+                <hr />
+                <div class="stats-container">
+                    <div class="stats-item">
+                        <h6>AVG. DISTANCE</h6>
+                        <h5>{{ destinations[activeView].distance }}</h5>
+                    </div>
+                    <div class="stats-item">
+                        <h6>Est. travel time</h6>
+                        <h5>{{ destinations[activeView].travel }}</h5>
+                    </div>
+                </div>
+            </div>
         </div>
         <div class="background" />
     </div>
@@ -75,5 +84,38 @@ button {
     background-image: url("../assets/destination/background-destination-mobile.jpg");
 }
 
-@media (min-width: 1024px) {}
+
+@media (min-width: 768px) {
+    .stats-container {
+        display: flex;
+        justify-content: space-around;
+    }
+
+    img {
+        width: 300px;
+        height: 300px;
+    }
+}
+
+
+@media (min-width: 1440px) {
+    .container {
+        display: flex;
+        justify-content: space-between;
+    }
+
+    .item,
+    .stats-item {
+        width: 50%;
+    }
+
+    ol {
+        margin-left: 0;
+    }
+
+    img {
+        width: 480px;
+        height: 480px;
+    }
+}
 </style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -7,10 +7,13 @@ const goToDestination = () => { router.push('/destination'); }
 
 <template>
   <div class="home">
-    <h4>SO, YOU WANT TO TRAVEL TO</h4>
-    <h1>SPACE</h1>
-    <p>Let’s face it; if you want to go to space, you might as well genuinely go to outer space and not hover kind of on
-      the edge of it. Well sit back, and relax because we’ll give you a truly out of this world experience!</p>
+    <div class="text">
+      <h4>SO, YOU WANT TO TRAVEL TO</h4>
+      <h1>SPACE</h1>
+      <p>Let’s face it; if you want to go to space, you might as well genuinely go to outer space and not hover kind of
+        on
+        the edge of it. Well sit back, and relax because we’ll give you a truly out of this world experience!</p>
+    </div>
     <button @click="goToDestination">
       <h3>EXPLORE</h3>
     </button>
@@ -49,5 +52,23 @@ button>h3 {
 
 .background {
   background-image: url("../assets/home/background-home-mobile.jpg");
+}
+
+@media (min-width: 768px) {
+  button {
+    width: 272px;
+    height: 272px;
+  }
+}
+
+@media (min-width: 1440px) {
+  .home {
+    display: flex;
+    justify-content: space-between;
+  }
+
+  .text {
+    width: 540px;
+  }
 }
 </style>
